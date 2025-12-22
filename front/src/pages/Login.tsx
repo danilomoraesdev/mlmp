ed75c7a339256b54fdf804833f94c475328e8967
s <s>
2025-12-22 02:21:51 -0300

    style: Update logo SVG stroke to currentColor and adjust login page logo container background and size.

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -6,7 +6,7 @@ export function LoginPage() {
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+          <div className="bg-primary/10 text-primary-foreground flex size-7 items-center justify-center rounded-md">
             <Logo />
           </div>
           MLMP
