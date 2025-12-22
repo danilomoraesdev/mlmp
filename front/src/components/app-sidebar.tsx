d060cca674086cc0d4be6b3ef54fe20320834191
s <s>
2025-12-21 21:46:31 -0300

    feat: Introduce new UI components including dropdown, tooltip, sheet, sidebar, and breadcrumbs, along with their Radix UI dependencies.

---
+++ front/src/components/app-sidebar.tsx
@@ -0,0 +1,102 @@
+import {
+  Sidebar,
+  SidebarRail,
+  SidebarMenu,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarContent,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+} from "@/components/ui/sidebar"
+import { Network } from "lucide-react"
+
+const data = {
+  userNav: [
+    {
+      title: "Rede",
+      url: "/rede",
+      items: [
+        {
+          title: "Resumo",
+          url: "/resumo",
+        },
+        {
+          title: "Matriz",
+          url: "/matriz",
+          isActive: true,
+        },
+        {
+          title: "Downline",
+          url: "/downline",
+        },
+      ],
+    },
+    {
+      title: "Sistema",
+      url: "/sistema",
+      items: [
+        {
+          title: "Minha Conta",
+          url: "/minha-conta",
+        },
+        {
+          title: "Financeiro",
+          url: "/financeiro",
+        },
+        {
+          title: "Configurações",
+          url: "/configuracoes",
+        },
+        {
+          title: "Sair",
+          url: "/logout",
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="/">
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <Network className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-medium">MLMP</span>
+                  <span className="">Painel do usuário</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        {data.userNav.map((group) => (
+          <SidebarGroup key={group.title}>
+            <SidebarGroupLabel>{group.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {group.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild isActive={item.isActive}>
+                      <a href={`${group.url}${item.url}`}>{item.title}</a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
